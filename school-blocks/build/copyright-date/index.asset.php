4d26494f888f247774bd39e9565130de3eb46156
MIKE <miket01927@gmail.com>
2026-02-23 21:17:23 -0800

    pushing copyright block again... think i made mistake first time

---
+++ school-blocks/build/copyright-date/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'd160dcc017284b9747b3');
---
+++ school-blocks/build/copyright-date/render.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * PHP file to use when rendering the block type on the server to show on the front end.
+ *
+ * The following variables are exposed to the file:
+ *     $attributes (array): The block attributes.
+ *     $content (string): The block default content.
+ *     $block (WP_Block): The block instance.
+ *
+ * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
+ */
+?>
+
+<p <?php echo get_block_wrapper_attributes(); ?>>
+	<?php esc_html_e( 'Copyright', 'copyright-date' ); ?> 
+	© 
+	<?php echo esc_html($attributes[ 'startingYear' ] ); ?> 
+	- 
+	<?php echo esc_html( date( 'Y' ) ); ?>
+</p>	
