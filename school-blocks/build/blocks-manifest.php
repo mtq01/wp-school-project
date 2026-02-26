276b4d984b0714d0c72fea4b31322c2048effcdd
MIKE <miket01927@gmail.com>
2026-02-25 21:58:53 -0800

    added Animation Block, Finished 2 CPT & Their Taxonomies.

--- school-blocks/build/blocks-manifest.php
+++ school-blocks/build/blocks-manifest.php
@@ -1,6 +1,30 @@
 <?php
 // This file is generated. Do not modify it manually.
 return array(
+	'animate-onscroll' => array(
+		'$schema' => 'https://schemas.wp.org/trunk/block.json',
+		'apiVersion' => 3,
+		'name' => 'school-blocks/animate-onscroll',
+		'version' => '0.1.0',
+		'title' => 'Animate',
+		'category' => 'design',
+		'icon' => 'buddicons-activity',
+		'description' => 'This block will animate when scrolled into the viewport.',
+		'attributes' => array(
+			'animation' => array(
+				'type' => 'string',
+				'default' => 'fade-up'
+			)
+		),
+		'supports' => array(
+			'html' => false
+		),
+		'textdomain' => 'school-blocks',
+		'editorScript' => 'file:./index.js',
+		'style' => 'file:./style-index.css',
+		'viewScript' => 'file:./view.js',
+		'editorStyle' => 'file:./index.css'
+	),
 	'copyright-date' => array(
 		'$schema' => 'https://schemas.wp.org/trunk/block.json',
 		'apiVersion' => 3,
--- school-blocks/build/copyright-date/index.asset.php
+++ school-blocks/build/copyright-date/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '3747f283b3049bee7838');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '6152fc8fc559da57e560');
